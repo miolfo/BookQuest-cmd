5a2fcb19a1c01b782d8364627999828afe73aad1
miolfo <mikko.o.forsman@gmail.com>
2023-03-02 20:10:28 +0200

    Fetch book info from finna api as json

--- pkg/finna/finna.go
+++ pkg/finna/finna.go
@@ -2,6 +2,9 @@ package finna
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	url2 "net/url"
 )
 
@@ -16,11 +19,33 @@ const finnaApiLookForTitle = "lookfor=title:"
 
 func FindBookByTitle(searchParameters SearchParameters) {
 
-	url := fmt.Sprintf("%s?%s%s&%s%s&sort=relevance:id asc&page=1",
+	url := fmt.Sprintf("%s?%s%s&%s%s",
 		finnaApiUrl,
 		finnaApiLookForTitle,
 		url2.QueryEscape(searchParameters.Title),
 		finnaApiFilterBuilding,
-		searchParameters.Building)
+		url2.QueryEscape(searchParameters.Building))
 	fmt.Println(url)
+	request := get(url)
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		log.Print("Unable to fetch book " + searchParameters.Title)
+		return
+	}
+	defer response.Body.Close()
+
+	json, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(json))
+}
+
+func get(url string) *http.Request {
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Host", "api.finna.fi")
+	request.Header.Set("Accept", "text/json")
+	return request
 }
